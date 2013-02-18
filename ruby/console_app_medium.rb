fb1f7bcea674b2990b5e3e4f6d1f5c2ac8a7eeed
Juanma Rodriguez <jmrod4@gmail.com>
2013-02-18 10:53:02 +0100

    Make it more the ruby way (following optparse library example)

--- ruby/console_app_medium.rb
+++ ruby/console_app_medium.rb
@@ -1,35 +1,66 @@
-# console_app_medium
-# $Id: console_app_medium.rb, v 0.1 2011/07/15
-# Copyright 2011 Juan M. Rodriguez 
+# console_app_medium - skeleton for shell application, medium complexity
+# Copyright 2011-2013 Juan M. Rodriguez
+#
 # You can redistribute and/or modify this software under the terms of GNU GPL v3 (or above) license
+#
+# Changelog
+# ---------
+# 0.1 - Initial version.
+# 0.2 - Make it more the ruby way (following optparse library example)
 
-Version = '0.1'
-Release = '2011/07/15'
+Version = '0.2'
+Release = '2013/02/18'
 
 
 require 'optparse'
 
-if __FILE__ == $0
-  options = {}
-  OptionParser.new do |opts|
-    opts.banner = "Usage: #{File.basename($0)} [options] arg1"
 
-    opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
-      options[:verbose] = v
+=begin
+usage example:
+  options = MyOptions.parse!(ARGV)
+=end
+class MyOptions
+
+  # class method, invoke with MyOptions.parse!
+  def self.parse!(args)
+    options = {}
+    
+    opt_parser = OptionParser.new do |opts|
+      opts.banner = "Usage: #{File.basename($0)} [options] arg1"
+
+      opts.on("-d", "--[no-]debug", "Run in debug mode") do |d|
+        options[:debug] = d
+      end
+    
+      opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+        options[:verbose] = v
+      end
+    
+      opts.on("-V", "--version", "Display version") do
+        puts opts.version() + ' ' + opts.release
+        exit
+      end
+  
     end
     
-    opts.on("-V", "--version", "Display version") do
-      puts opts.version() + ' ' + opts.release
+    opt_parser.parse!(args)
+
+    if args.size != 1
+      opt_parser.warn('one argument required')
       exit
     end
     
-    if ARGV.size != 1
-      opts.warn('one argument required')
-      exit
-    end
-    
-  end.parse!
+    options
+  end
 
+end
+
+
+options = MyOptions.parse!(ARGV)
+
+if options[:debug]
+  print "Options: "
   p options
+  print "Arguments: "
   p ARGV
 end
