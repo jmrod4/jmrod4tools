9b343a6fa7af40b3447f28e4477f2f630bde52ed
Juanma Rodriguez <jmrod4@gmail.com>
2013-06-01 12:40:08 +0200

    minor modifications in the shell, examples and tips, github tips and README

--- README.markdown
+++ README.markdown
@@ -9,8 +9,14 @@ This project is a collection of **very simple** tools and tips for:
 - Shell Scripts
 - git
 
-At the moment I use them myself mostly as memory hooks. But who knows, they could eventually evolve into something more ambitious.
+At the moment I use them myself mostly as memory hooks for myself. It's 
+unlikely it could eventually evolve into something more ambitious but who 
+knows.
 
-If you find something useful here just let me know about it. It could lead to future enhancements or, more probably, just to a morale boost. Also if you want to contribute something or even forking the full project by all means do so.
+If you find something useful here just you can let me know about it. 
+It could lead to a morale boost. Also if you want to contribute something or 
+even forking the full project then by all means do so.
 
+Peace.
 
+Juanma
--- shell/shell_tips.sh
+++ shell/shell_tips.sh
@@ -1,42 +1,44 @@
 #!/bin/bash
 
 
+# assign variables
 I=20
 let I-=1
 
 
-
-ls -d flac/ACDC* | while read fname;do
+# play some sound files...
+ls -d ~/flac/ACDC* | while read fname;do
 	mp3std "$fname"
-done 
+done
 
 
-
+# some expressions
 echo $(ls)
-echo $((1+1)) 
-echo $[1+1] 
+echo `ls`	# same output than previous command
+echo $((1+1))
+echo $[1+1]	# same output than previous command
 echo $[3/4]
-echo 3/4|bc -l
+echo 3/4|bc -l	# ????
 
 
-
+# simple option processing 
+# (see console_app_simple.sh file included in this same project)
 OPTIONS="A B C"
 select opt in $OPTIONS;do 
 	echo $opt
 done
 
 
-
+# simple input
 echo Please, enter your firstname and lastname
-read FN LN 
-echo "Hi! $LN, $FN !"
-
+read FN LN
+echo "Hi! ${LN}, ${FN} !"
 
 
 echo $?		# return value of last program, 0=success normally
 
 
-
+# interesting commands !!
 man sed
 man awk
 man grep
@@ -46,53 +48,55 @@ man bc
 man tput
 
 
-
+# simple bach processing console commands
 tput -S <<!
 clear
 cup 10 10
 bold
 !
-echo kkfuti
+# restore the terminal
+reset
 
 
-
-# Run as root, of course.
+# check if the user running the script has root privileges
 ROOT_UID=0     # Only users with $UID 0 have root privileges.
 E_NOTROOT=67   # Non-root exit error.
 if [ "$UID" -ne "$ROOT_UID" ]
 then
   echo "Must be root to run this script."
   exit $E_NOTROOT
-fi 
+fi
 
 
-
-E_XCD=66       # Can't change directory?
+# simple checking and error output
+E_XCD=66       # can't change directory error
 cd /var/logg || {
    echo "Cannot change to necessary directory." >&2
    exit $E_XCD;
 }
 
 
-
+# more error codes
 E_WRONG_ARGS=65
 
 
-
+# simple dated logging
 echo "`date +'%Y-%m-%d %H:%M:%S'` Some log info" >> log.txt
 
 
-
-: > data.xxx   # File "data.xxx" now empty (created if not exist, empty with same permisions if already existed)	      
-# Same effect as   cat /dev/null >data.xxx
+# provides an empty file
+# * if it doesn't exist then it's created empty
+# * if it already existed it now it empty with the same permisions it had
+: > data.bin
+# previous command has the same effect as cat /dev/null >data.xxx
 # (jmr: and maybe the same as touch data.xxx)
-# However, this does not fork a new process, since ":" is a builtin.
+# the difference is that this does not fork a new process, since ":" is a builtin
 
 
 
 #!/bin/bash
 # renames.sh
-# basic file renamer
+# basic file renamer with regular expressions
 criteria=$1
 re_match=$2
 replace=$3
@@ -105,8 +109,12 @@ done
 
 
 
+
+# alternative form of checking a command exit code
 if COMMAND_WHOSE_EXIT_STATUS_IS_0_UNLESS_ERROR_OCCURRED
 then echo "Command succeeded."
 else echo "Command failed."
 fi
+
+
 
